98abc9ca4972e722f94429b28a5f76fd879f1919
reesaespera <80938415+reesaespera@users.noreply.github.com>
2023-08-07 12:42:59 -0500

    edits

---
+++ LMM/nn.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -N NE_NET
+#$ -cwd
+#$ -o nn.out
+#$ -e nn.err
+#$ -pe orte 12
+#$ -q intel24
+module load openmpi/3.1.4
+module load anaconda/3-2020.07
+
+python nn.ipy
---
+++ Non-Fullerene Dataset/nf_nn_lumo.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -N NF_NN
+#$ -cwd
+#$ -o nf_nn_l.out
+#$ -e nf_nn_l.err
+#$ -pe orte 12
+#$ -q intel24
+module load openmpi/3.1.4
+module load anaconda/3-2020.07
+
+python nf_nn_lumo.ipy
